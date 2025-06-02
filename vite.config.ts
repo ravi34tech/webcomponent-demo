27a443230d1941e93f39ee138721adc9247df651
Ravi Mummadu <rmummadu@thelcfgroup.com>
2025-06-02 14:13:27 +0530

    build changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/webcomponent-demo/',
   define: {
     "process.env": {
       NODE_ENV: "production",
     },
   },
   plugins: [react()],
+  
 
   // 👇 Insert these lines
   build: {
